2dc947f73aae87d55142a2bde53801c48f07b12a
Joe Sak <joe@joesak.com>
2015-06-12 11:43:34 -0400

    Update how it gets set

--- lib/lev.rb
+++ lib/lev.rb
@@ -62,7 +62,7 @@ module Lev
         @security_transgression_error = Lev::SecurityTransgression
         @illegal_argument_error = Lev::IllegalArgument
         @raise_fatal_errors = false
-        @active_job_class = ActiveJob::Base if defined?(ActiveJob)
+        @active_job_class = defined?(ActiveJob) ? ActiveJob::Base : nil
         super
       end
     end
