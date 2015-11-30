2216ab683d1a6c1d618ff0030907139586f7109f
Joe Sak <joe@joesak.com>
2015-11-29 22:57:34 -0500

    Refactor to fix nested routine setup bug

--- lib/lev/utils/nested_routines.rb
+++ lib/lev/utils/nested_routines.rb
@@ -2,11 +2,14 @@ module Lev
   module Utils
     class NestedRoutines
       def self.setup(routine_class, intended_nested_routines)
-        [intended_nested_routines].flatten.compact.each do |nested_routine|
-          key = Symbolify.exec(nested_routine)
-          name = Nameify.exec(nested_routine)
+        [intended_nested_routines].flatten.each do |src|
+          key = Symbolify.exec(src)
+          name = Nameify.exec(src)
+          name_alias = Aliasify.exec(src)
 
-          routine_class.nested_routines[key] ||= { routine_class: name, attributes: [] }
+          routine_class.nested_routines[key] ||= { name_alias: name_alias,
+                                                   routine_class: name,
+                                                   attributes: Set.new }
         end
       end
     end
--- lib/lev/utils/outputs.rb
+++ lib/lev/utils/outputs.rb
@@ -6,39 +6,34 @@ module Lev
         routine_class.define_singleton_method('outputs') { @outputs }
 
         nested_map = routine_class.outputs.select { |_, source| source != :_self }
-        setup_nested_routine_outputs(routine_class.nested_routines, nested_map)
+        setup_nested_routine_outputs(routine_class, nested_map)
       end
 
       private
-      def self.setup_nested_routine_outputs(nested_routines, map)
+      def self.setup_nested_routine_outputs(routine_class, map)
         map.each do |attribute, source|
+          NestedRoutines.setup(routine_class, source)
+
           [source].flatten.each do |src|
             key = Symbolify.exec(src)
-            name = Nameify.exec(src)
-            name_alias = Aliasify.exec(src)
-
-            nested_routines[key] ||= { name_alias: name_alias,
-                                       routine_class: name,
-                                       attributes: Set.new }
-
-            map_attribute(nested_routines, key, attribute)
+            map_attribute(routine_class, key, attribute)
           end
         end
       end
 
-      def self.map_attribute(nested_routines, key, attribute)
+      def self.map_attribute(routine_class, key, attribute)
         case attribute
         when :_verbatim
-          promote_verbatim_attributes(nested_routines, key)
+          promote_verbatim_attributes(routine_class, key)
         else
-          nested_routines[key][:attributes] << attribute
+          routine_class.nested_routines[key][:attributes] << attribute
         end
       end
 
       # TODO: This is so bad
       # blame joemsak
-      def self.promote_verbatim_attributes(nested_routines, key)
-        nested_class = nested_routines[key][:routine_class]
+      def self.promote_verbatim_attributes(routine_class, key)
+        nested_class = routine_class.nested_routines[key][:routine_class]
         map = nested_class.outputs
         sub_map = {}
 
@@ -46,7 +41,7 @@ module Lev
           construct_map(map, attr, source, sub_map, nested_class)
         end
 
-        setup_nested_routine_outputs(nested_routines, map.merge(sub_map))
+        setup_nested_routine_outputs(routine_class, map.merge(sub_map))
       end
 
       def self.construct_map(map, attr, source, sub_map, nested_class)
