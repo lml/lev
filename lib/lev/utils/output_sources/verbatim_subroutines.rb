a01113c34f32728fc153980f78c279f9cdd95a21
Joe Sak <joe@joesak.com>
2015-12-02 16:36:32 -0500

    Refactor mercilessly

--- lib/lev/utils/output_sources/verbatim_subroutines.rb
+++ lib/lev/utils/output_sources/verbatim_subroutines.rb
@@ -3,26 +3,23 @@ module Lev
     module OutputSources
       class VerbatimSubroutines
         def self.setup(routine_class, map)
-          map.each do |_, source|
+          map.values.each do |source|
             Uses.setup(routine_class, source)
+            promote_verbatim_attributes(routine_class, nil, source)
+          end
+        end
 
-            [source].flatten.each do |src|
-              key = Symbolify.exec(src)
-              nested_class = Nameify.exec(src)
+        private
+        def self.promote_verbatim_attributes(routine_class, key, source)
+          [source].flatten.each do |src|
+            key ||= Symbolify.exec(src)
+            nested_class = Nameify.exec(src)
 
-              attrs = nested_class.outputs.select { |k, _| k != :_verbatim }.keys
-              verbatims = [nested_class.outputs[:_verbatim]].flatten.compact
+            attrs = nested_class.outputs.select { |k, _| k != :_verbatim }.keys
+            routine_class.subroutines[key][:attributes] += attrs
 
-              routine_class.subroutines[key][:attributes] += attrs
-
-              verbatims.each do |verbatim_source|
-                verbatim_class = Nameify.exec(verbatim_source)
-                attrs = verbatim_class.outputs.select { |k, _| k != :_verbatim }.keys
-                verbatims = [verbatim_class.outputs[:_verbatim]].flatten.compact
-
-                routine_class.subroutines[key][:attributes] += attrs
-              end
-            end
+            verbatims = [nested_class.outputs[:_verbatim]].flatten.compact
+            promote_verbatim_attributes(routine_class, key, verbatims)
           end
         end
       end
