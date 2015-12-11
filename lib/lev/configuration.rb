19329935eddeda3d86994152f35d3f97b6837d1a
Joe Sak <joe@joesak.com>
2015-12-11 11:43:35 -0500

    Raising fatal errors is false by default

--- lib/lev/configuration.rb
+++ lib/lev/configuration.rb
@@ -3,7 +3,7 @@ module Lev
     attr_accessor :raise_fatal_errors, :job_store, :job_store_namespace
 
     def initialize
-      @raise_fatal_errors = true
+      @raise_fatal_errors = false
       @job_store = Lev::MemoryStore.new
       @job_store_namespace = :lev_job
     end
