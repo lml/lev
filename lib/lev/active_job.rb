3fb022d88e2624eab530d72a938f1f4440d693a6
JP Slavinsky <jpslav@gmail.com>
2016-01-14 19:43:05 -0800

    indentation

--- lib/lev/active_job.rb
+++ lib/lev/active_job.rb
@@ -5,18 +5,18 @@ module Lev
         queue_as routine_class.active_job_queue
         args.push(routine_class.to_s)
 
-          # Create a new status object and push its ID on to the arguments so that
-          # in `perform` it can be used to retrieve the status when the routine is
-          # initialized.
-          status = Lev::create_status
-          args.push(status.id)
+        # Create a new status object and push its ID on to the arguments so that
+        # in `perform` it can be used to retrieve the status when the routine is
+        # initialized.
+        status = Lev::create_status
+        args.push(status.id)
 
         # In theory we'd mark as queued right after the call to super, but this messes
         # up when the activejob adapter runs the job right away
-          status.queued!
+        status.queued!
         super(*args, &block)
 
-          status.id
+        status.id
       end
 
       def perform(*args, &block)
@@ -24,7 +24,7 @@ module Lev
         id = args.pop
         routine_class = Kernel.const_get(args.pop)
 
-          routine_instance = routine_class.new(Lev::find_status(id))
+        routine_instance = routine_class.new(Lev::find_status(id))
 
         routine_instance.call(*args, &block)
       end
